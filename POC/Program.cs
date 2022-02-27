3f478362eaf26cae3427f37cc9161a6980864f93
Arslan.Riaz <arslan.riaz@visionetsystems.com>
2022-02-28 03:45:12 +0500

    added Edi files Inside

--- POC/Program.cs
+++ POC/Program.cs
@@ -15,10 +15,27 @@ namespace POC
         static void Main(string[] args)
         {
             var listEdiFiles = new List<Tuple<string, string,string,string>>();
-            listEdiFiles.Add(Tuple.Create(@"C:\Users\Dell\Desktop\EdiWork\ListLoad850.txt", @"C:\Users\Dell\Desktop\EdiWork\Packing_ORIGINAL_850.xml", @"C:\Users\Dell\Desktop\EdiWork\XpathLoad850.html", @"C:\Users\Dell\Desktop\EdiWork\XpathLoad850Updated.html"));
-            listEdiFiles.Add(Tuple.Create(@"C:\Users\Dell\Desktop\EdiWork\ListLoad856.txt", @"C:\Users\Dell\Desktop\EdiWork\Packing_ORIGINAL_856.xml", @"C:\Users\Dell\Desktop\EdiWork\XpathLoad856.html", @"C:\Users\Dell\Desktop\EdiWork\XpathLoad856Updated.html"));
-            listEdiFiles.Add(Tuple.Create(@"C:\Users\Dell\Desktop\EdiWork\ListLoad990.txt", @"C:\Users\Dell\Desktop\EdiWork\Packing_ORIGINAL_990.xml", @"C:\Users\Dell\Desktop\EdiWork\XpathLoad990.html", @"C:\Users\Dell\Desktop\EdiWork\XpathLoad990Updated.html"));
-            listEdiFiles.Add(Tuple.Create(@"C:\Users\Dell\Desktop\EdiWork\ListLoad810.txt", @"C:\Users\Dell\Desktop\EdiWork\Packing_ORIGINAL_810.xml", @"C:\Users\Dell\Desktop\EdiWork\XpathLoad810.html", @"C:\Users\Dell\Desktop\EdiWork\XpathLoad810Updated.html"));
+            Console.WriteLine(Path.Combine(Directory.GetParent(System.IO.Directory.GetCurrentDirectory()).Parent.Parent.Parent.FullName, @"EdiWork\ListLoad850.txt"));
+            listEdiFiles.Add(Tuple.Create(
+                Path.Combine(Directory.GetParent(System.IO.Directory.GetCurrentDirectory()).Parent.Parent.Parent.FullName, @"EdiWork\ListLoad850.txt"),
+                Path.Combine(Directory.GetParent(System.IO.Directory.GetCurrentDirectory()).Parent.Parent.Parent.FullName, @"EdiWork\Packing_ORIGINAL_850.xml"),
+                Path.Combine(Directory.GetParent(System.IO.Directory.GetCurrentDirectory()).Parent.Parent.Parent.FullName, @"EdiWork\XpathLoad850.html"),
+                Path.Combine(Directory.GetParent(System.IO.Directory.GetCurrentDirectory()).Parent.Parent.Parent.FullName, @"EdiWork\XpathLoad850Updated.html")));
+            listEdiFiles.Add(Tuple.Create(
+                Path.Combine(Directory.GetParent(System.IO.Directory.GetCurrentDirectory()).Parent.Parent.Parent.FullName, @"EdiWork\ListLoad856.txt"),
+                Path.Combine(Directory.GetParent(System.IO.Directory.GetCurrentDirectory()).Parent.Parent.Parent.FullName, @"EdiWork\Packing_ORIGINAL_856.xml"),
+                Path.Combine(Directory.GetParent(System.IO.Directory.GetCurrentDirectory()).Parent.Parent.Parent.FullName, @"EdiWork\XpathLoad856.html"),
+                Path.Combine(Directory.GetParent(System.IO.Directory.GetCurrentDirectory()).Parent.Parent.Parent.FullName, @"EdiWork\XpathLoad856Updated.html")));
+            listEdiFiles.Add(Tuple.Create(
+                Path.Combine(Directory.GetParent(System.IO.Directory.GetCurrentDirectory()).Parent.Parent.Parent.FullName, @"EdiWork\ListLoad810.txt"),
+                Path.Combine(Directory.GetParent(System.IO.Directory.GetCurrentDirectory()).Parent.Parent.Parent.FullName, @"EdiWork\Packing_ORIGINAL_810.xml"),
+                Path.Combine(Directory.GetParent(System.IO.Directory.GetCurrentDirectory()).Parent.Parent.Parent.FullName, @"EdiWork\XpathLoad810.html"),
+                Path.Combine(Directory.GetParent(System.IO.Directory.GetCurrentDirectory()).Parent.Parent.Parent.FullName, @"EdiWork\XpathLoad810Updated.html")));
+            listEdiFiles.Add(Tuple.Create(
+                Path.Combine(Directory.GetParent(System.IO.Directory.GetCurrentDirectory()).Parent.Parent.Parent.FullName, @"EdiWork\ListLoad990.txt"),
+                Path.Combine(Directory.GetParent(System.IO.Directory.GetCurrentDirectory()).Parent.Parent.Parent.FullName, @"EdiWork\Packing_ORIGINAL_990.xml"),
+                Path.Combine(Directory.GetParent(System.IO.Directory.GetCurrentDirectory()).Parent.Parent.Parent.FullName, @"EdiWork\XpathLoad990.html"),
+                Path.Combine(Directory.GetParent(System.IO.Directory.GetCurrentDirectory()).Parent.Parent.Parent.FullName, @"EdiWork\XpathLoad990Updated.html")));
             XPathHelper.XPathMapper(listEdiFiles);
         }
         
